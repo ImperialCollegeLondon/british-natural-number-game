6c0d33ce84ac73b3da9949b1ea8704d7bf4c174c
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-08-07 23:10:55 +0100

    adding test file

---
+++ src/test.lean
@@ -0,0 +1,9 @@
+import data.real.basic
+
+-- the game is: how quickly can we get the user playing the game.
+-- The answer to this level is "change `sorry` to `refl`.
+
+example : (2 : ℝ) = (2 : ℝ) :=
+begin
+  sorry
+end
