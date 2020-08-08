4719fd80dbd6e0d4d67b479d415a74e7e3f34534
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-08-08 06:35:21 +0100

    add 37 example

--- src/test.lean
+++ src/test.lean
@@ -3,7 +3,7 @@ import data.real.basic
 -- the game is: how quickly can we get the user playing the game.
 -- The answer to this level is "change `sorry` to `refl`.
 
-example : (2 : ℝ) = (2 : ℝ) :=
+example : (37 : ℝ) = (37 : ℝ) :=
 begin
   sorry
 end
